548a1b5451ee4758eff6673875bf47130e9feab8
Tom Williams <tom.williams0593@outlook.com>
2025-11-28 03:23:28 +0000

    TDD: Support creating users.

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,7 +3,7 @@ Django admin customization.
 """
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.utils.translation import gettext_lazy as _ # Future proofing
+from django.utils.translation import gettext_lazy as _  # Future proofing
 
 from core import models
 
@@ -27,6 +27,20 @@ class UserAdmin(BaseUserAdmin):
         (_('Important dates'), {'fields': ('last_login',)}),
     )
     readonly_fields = ['last_login']
-
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'first_name',
+                'last_name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
 
 admin.site.register(models.User, UserAdmin)
