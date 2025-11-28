c13912fa49db00044b012d23ec3642249a4a3f3d
Tom Williams <tom.williams0593@outlook.com>
2025-11-28 02:38:59 +0000

    Make Django admin list users.

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -21,7 +21,8 @@ class AdminSiteTests(TestCase):
         self.user = get_user_model().objects.create_user(
             email='user@example.com',
             password='testpass123',
-            name='Test User'
+            first_name='Test',
+            last_name='User'
         )
 
     def test_users_lists(self):
@@ -29,5 +30,6 @@ class AdminSiteTests(TestCase):
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
 
-        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.first_name)
+        self.assertContains(res, self.user.last_name)
         self.assertContains(res, self.user.email)
