5a00e46be95a8519ba41aa17f9b792042d1e098b
Tom Williams <tom.williams0593@outlook.com>
2025-11-28 14:21:20 +0000

    Configuring drf spectacular for swagger UI testing.

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,6 +17,7 @@ from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
+
 from django.contrib import admin
 from django.urls import path
 
@@ -26,6 +27,6 @@ urlpatterns = [
     path(
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
-        name='api-docs'
+        name='api-docs',
     )
 ]
